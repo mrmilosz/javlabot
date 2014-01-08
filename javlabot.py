59ff6a2833520164d2f251cdd48e647b3517bd70
mrmilosz <milosz@milosz.ca>
2014-01-08 18:21:55 -0500

    Added a quit message, made the string building more pythonic

--- javlabot.py
+++ javlabot.py
@@ -19,31 +19,27 @@ turkey_critical_mass = 20
 
 # Each line from the server represents a message, which we should somehow handle
 def handle_message(message):
-	try:
-		# First we have to get the ping out of the way
-		if message.startswith('PING'):
-			send('PONG ' + re.sub(r'[^\d]*', '', message))
+	# First we have to get the ping out of the way
+	if message.startswith('PING'):
+		send('PONG %s' % re.sub(r'[^\d]*', '', message))
 
-		elif message.startswith(':'):
-			sender, code, body = message.split(' ', 2)
+	elif message.startswith(':'):
+		sender, code, body = message.split(' ', 2)
 
-			# 001 is the welcome code, and it means we can join channels
-			if code == '001':
-				send('JOIN ' + channel)
+		# 001 is the welcome code, and it means we can join channels
+		if code == '001':
+			send('JOIN %s' % channel)
 
-			# PRIVMSG is the turkey-hunting code
-			elif code == 'PRIVMSG':
-				global turkeys
-				turkey_name = re.match(r':([^!]*)!', sender).group(1)	
-				if not turkey_name in turkeys:
-					turkeys[turkey_name] = 0
-				turkeys[turkey_name] += 1
-				if turkeys[turkey_name] > turkey_critical_mass:
-					send('PRIVMSG %s :jävla %s' % (channel, turkey_name))
-					turkeys = {}
-
-	except Exception as e:
-		print('Exception: %s' % e)
+		# PRIVMSG is the turkey-hunting code
+		elif code == 'PRIVMSG':
+			global turkeys
+			turkey_name = re.match(r':([^!]*)!', sender).group(1)	
+			if not turkey_name in turkeys:
+				turkeys[turkey_name] = 0
+			turkeys[turkey_name] += 1
+			if turkeys[turkey_name] > turkey_critical_mass:
+				send('PRIVMSG %s :jävla %s' % (channel, turkey_name))
+				turkeys = {}
 
 # This is a blocking function, which listens for data from the IRC server forever
 def listen():
@@ -55,18 +51,21 @@ def listen():
 		data_buffer = lines.pop()
 
 		for message in lines:
-			print('SERVER: ' + message)
-			handle_message(message)
+			print('SERVER: %s' % message)
+			try:
+				handle_message(message)
+			except Exception as e:
+				print('Exception: %s' % e)
 
 # Go through here to send messages to the IRC server
 def send(message):
-	print('CLIENT: ' + message)
+	print('CLIENT: %s' % message)
 	irc.send(bytes(message + '\r\n', 'UTF-8'))
 
 # Make sure to disconnect cleanly on program exit
 def exit_gracefully(signal, frame):
 	print()
-	send('QUIT')
+	send('QUIT jävla %s' % username)
 	sys.exit(0)
 
 
@@ -76,7 +75,7 @@ irc.connect((network, port))
 signal.signal(signal.SIGINT, exit_gracefully)
 
 # Identify
-send('NICK %s' % (username))
+send('NICK %s' % username)
 send('USER %s 0 * :%s' % (username, realname))
 
 # Start botting
